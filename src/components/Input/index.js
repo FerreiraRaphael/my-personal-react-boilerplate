57563ddab1b1e71c48ba883b2673df3d7e0e8046
raphael ferreira <raphaelbsferreira@gmail.com>
2017-03-17 04:57:30 -0300

    test pre commit

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import styles from './styles.css'
 
 const Input = props =>
-    <input style={styles.container} onChange={(e) => props.onChange(e.target.value)} />
+      <input style={styles.container} onChange={(e) => props.onChange(e.target.value)} />
 
 Input.propTypes = {
   onChange: PropTypes.func.isRequired
