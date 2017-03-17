e0a80e5fe1716eda722f449c01ba0d18c69aada9
raphael ferreira <raphaelbsferreira@gmail.com>
2017-03-17 05:02:39 -0300

    linter fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from 'react'
 import Input from './components/Input'
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor (props) {
     super()
     this.state = {
-      value: ""
+      value: ''
     }
     this.onChange = this.onChange.bind(this)
   }
-  onChange(value){
-    this.setState({value})
+  onChange (value) {
+    this.setState({ value })
   }
-  render() {
+  render () {
     return (
-      <div style={{textAlign: 'center'}}>
+      <div style={{ textAlign: 'center' }}>
         <h1>Hello World!</h1>
         <h3>{this.state.value}</h3>
-        <Input onChange={this.onChange}/>
+        <Input onChange={this.onChange} />
       </div>
     )
   }
